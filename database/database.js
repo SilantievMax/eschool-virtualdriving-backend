c3481d16ecf3a6f5ee89b52fb4f9b323c159f9e0
SilantievMax <nohrin.max@yandex.ru>
2022-11-15 01:00:25 +0300

    modified database.js

--- database/database.js
+++ database/database.js
@@ -3,11 +3,16 @@ import dotenv from "dotenv";
 
 dotenv.config();
 const LINK_DB = process.env.LINK_DB;
-const NAME_DB = process.env.NAME_DB;
 
 mongoose
-    .connect(LINK_DB + NAME_DB)
-    .then(() => console.log("DB OK"))
-    .catch((err) => console.log("DB error", err));
+    .connect(LINK_DB, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => console.log("OK - MongooDB "))
+    .catch((err) => {
+        console.log("DB error:", err.message);
+        process.exit(1);
+    });
 
 export default mongoose;
