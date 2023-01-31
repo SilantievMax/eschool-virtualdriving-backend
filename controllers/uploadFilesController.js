4d78aaa3fac556cac54449b38b42481a3db7cb8e
Kirill Gladkikh <onest.kill@yandex.ru>
2023-02-01 03:30:55 +0400

    upload file: description, imgCarFile, tracksData

--- controllers/uploadFilesController.js
+++ controllers/uploadFilesController.js
@@ -7,7 +7,7 @@ import FileModel from "../models/File.js";
 export const uploadfile = async (req, res) => {
   try {
     if (req.files.file) {
-      const { file, img } = req.files;
+      const { file, img, imgCar, tracksData } = req.files;
       const isFile = await FileModel.findOne({ nameDefault: file.name });
 
       if (isFile) {
@@ -15,22 +15,32 @@ export const uploadfile = async (req, res) => {
       }
 
       const name = req.body.name ? req.body.name : file.name.replace(".rar", "");
+      const description = req.body.description || ''
 
       const filePath = `uploads/${randomstring.generate(20)}.rar`;
+      // todo: перезаписываются файлы по названию
       const imgPath = img ? `uploads/img/${img.name.split(" ").join("_")}` : `uploads/img/default.png`;
+      const imgCarPath = imgCar ? `uploads/img/${imgCar.name.split(" ").join("_")}` : '';
 
       const staticFilePath = "/static/" + filePath;
       const staticImgPath = "/static/" + imgPath;
+      const staticImgCarPath = imgCarPath ? "/static/" + imgCarPath : '';
+
+      const buff = Buffer.from(tracksData.data);
+      const { data: tracks } = JSON.parse(buff.toString())
 
       const doc = new FileModel({
-        name: name,
+        name,
         nameDefault: file.name,
+        description,
         type: file.mimetype,
         size: file.size,
         user: req.userId,
         price: req.body.price,
         pathFile: staticFilePath,
         imgFile: staticImgPath,
+        imgCarFile: staticImgCarPath,
+        tracksData: tracks,
       });
 
       const fileSave = await doc.save();
@@ -53,7 +63,17 @@ export const uploadfile = async (req, res) => {
         });
       }
 
-      return res.status(200).json({ fileSave, message: "Файл загружен!" });
+      if (imgCar) {
+        imgCar.mv(imgCarPath, (err) => {
+          if (err) {
+            return res.status(500).json({
+              message: "Произошла ошибка при загрузке файла",
+            });
+          }
+        });
+      }
+
+      return res.status(201).json({ fileSave, message: "Файл загружен!" });
     } else {
       res.status(400).json({ message: "Файл не загружен!" });
     }
